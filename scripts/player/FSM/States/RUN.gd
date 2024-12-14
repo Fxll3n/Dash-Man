d88531cee0f5e5823e5a47da112a433d029ae0a3
Angel Bitsov <angel.bitsov@gmail.com>
2024-12-14 03:46:09 -0600

    Added sfx

--- scripts/player/FSM/States/RUN.gd
+++ scripts/player/FSM/States/RUN.gd
@@ -1,13 +1,18 @@
 extends State
 class_name RUN
 
+@onready var footstep_player: AudioStreamPlayer = $"../../FootstepPlayer"
+
 @export var moveSpeed := 340
+var sfx = preload("res://assets/sound/sounds/tap.wav")
+var didPlay = false
 var direction = 0
 var calculate_run_animation_fps = func() -> float:
 	return abs((moveSpeed*0.38) / sprite_2d.sprite_frames.get_frame_count("run"))
 
 func enter():
 	sprite_2d.play("run")
+	footstep_player.stream = sfx
 	sprite_2d.sprite_frames.set_animation_speed("run", calculate_run_animation_fps.call())
 
 func update(delta):
@@ -39,3 +44,10 @@ func animate():
 		sprite_2d.flip_h = true
 	elif direction > 0:
 		sprite_2d.flip_h = false
+	
+	if sprite_2d.frame % 2 == 0:
+		if !didPlay:
+			footstep_player.play()
+			didPlay = true
+	else:
+		didPlay = false
